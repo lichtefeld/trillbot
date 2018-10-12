15e9d551cab6219c03713738f984423554ac1ad1
Jacob Lichtefeld <LichMaster98@gmail.com>
2018-10-12 10:11:07 -0400

    explict channel over replyASync

--- Commands/GameCommands.cs
+++ Commands/GameCommands.cs
@@ -21,14 +21,14 @@ namespace trillbot.Commands
         public async Task initAsync() {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if (prix.Value != null) {
-                await ReplyAsync("Woah there, a game is already initialized in this channel. Try using `ta!reset` to reset this channel");
+                await Context.Channel.SendMessageAsync("Woah there, a game is already initialized in this channel. Try using `ta!reset` to reset this channel");
                 return;
             }
             var game = new GrandPrix {
                 channelName = Context.Channel.Name
             };
             Program.games.Add(Context.Channel.Id, game);
-            await ReplyAsync("Game started and bound to this channel use `ta!joingame` in this channel to join.");
+            await Context.Channel.SendMessageAsync("Game started and bound to this channel use `ta!joingame` in this channel to join.");
         }
 
         [Command("startgame")]
@@ -36,7 +36,7 @@ namespace trillbot.Commands
         public async Task startGame() {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else {
                 prix.Value.startGame(Context); //Consider checking for a player to be in the game, rather than the Administrator node
             }
@@ -46,7 +46,7 @@ namespace trillbot.Commands
         public async Task joinGame() {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.joinGame(Context);
             }
@@ -56,7 +56,7 @@ namespace trillbot.Commands
         public async Task discardAsync(int i) {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.discardAsync(Context, i);
             }
@@ -66,7 +66,7 @@ namespace trillbot.Commands
         public async Task playCardAsync(int i, int racerID = -1, int hazardID = -1) {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.playCardAsync(Context, i, racerID,hazardID);
             }
@@ -76,7 +76,7 @@ namespace trillbot.Commands
         public async Task inGameAsync() {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.inGameAsync(Context);
             }
@@ -87,7 +87,7 @@ namespace trillbot.Commands
         public async Task doReset() {
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.doReset(Context);
                 Program.games.Remove(Context.Channel.Id);
@@ -101,7 +101,7 @@ namespace trillbot.Commands
         public async Task whosTurnAsync() { //Need to remind a person its there turn?
             var prix = Program.games.ToList().FirstOrDefault(e=> e.Key == Context.Channel.Id);
             if ( prix.Value == null) {
-                await ReplyAsync("No game running in this channel. Initialize one with `ta!initialize`");
+                await Context.Channel.SendMessageAsync("No game running in this channel. Initialize one with `ta!initialize`");
             } else { 
                 prix.Value.whosTurnAsync(Context);
             }
