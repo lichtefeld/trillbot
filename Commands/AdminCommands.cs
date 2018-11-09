a0c1c3c4c126eee027598c1a7577e8834de88523
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-09 15:55:14 -0600

    adding api call to get stats on website

--- Commands/AdminCommands.cs
+++ Commands/AdminCommands.cs
@@ -12,54 +12,105 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using trillbot.Classes;
+using RestSharp;
 
 namespace trillbot.Commands
 {
-    public class AdminCommands : ModuleBase<SocketCommandContext> {
-        
+    public class leaderboard_stats
+    {
+        public string leaderboards { get; set; }
+        public string api_key { get; set; }
+    }
+    public class AdminCommands : ModuleBase<SocketCommandContext>
+    {
+
         [Command("add")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task addFundsAsync(SocketUser user, int i) {
+        public async Task addFundsAsync(SocketUser user, int i)
+        {
             var c = Character.get_character(user.Id);
-            if(c == null) {
+            if (c == null)
+            {
                 await ReplyAsync("This user doesn't have an account");
                 return;
             }
 
-            if (i <= 0) {
+            if (i <= 0)
+            {
                 await ReplyAsync("Don't add negative or 0 funds.");
                 return;
             }
 
-            c.balance+=i;
+            c.balance += i;
             await ReplyAsync(c.name + " has a new balance of " + c.balance);
             Character.update_character(c);
         }
 
+        [Command("updateleaderboards")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task updateleaderboardsAsync()
+        {
+            List<Classes.Character> characters = Classes.Character.get_character();
+
+            List<string> rtn = new List<string>();
+
+            characters.OrderByDescending(e=>e.balance).ToList().ForEach(e=>rtn.Add(e.name + " " + e.balance));
+
+
+            var file = "trillbot.json";
+
+            var secrets = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(file));
+
+            string key = secrets["api_key"];
+
+            leaderboard_stats s = new leaderboard_stats
+            {
+                leaderboards = string.Join(System.Environment.NewLine, rtn),
+                api_key = key
+            };
+
+            string baseurl = string.Concat("https://trilliantring.com/api/Updateleaderboard");
+            // string baseurl = string.Concat ("http://localhost:5000/api/UpdateMemCount");
+
+            var client = new RestClient(baseurl);
+
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("text/json", JsonConvert.SerializeObject(s), ParameterType.RequestBody);
+
+            request.AddHeader("Content-Type", "text/json");
+
+            var response = client.Execute(request);
+        }
+
         [Command("sub")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task subFundsAsync(SocketUser user, int i) {
+        public async Task subFundsAsync(SocketUser user, int i)
+        {
             var c = Character.get_character(user.Id);
-            if(c == null) {
+            if (c == null)
+            {
                 await ReplyAsync("This user doesn't have an account");
                 return;
             }
 
-            if (i <= 0) {
+            if (i <= 0)
+            {
                 await ReplyAsync("Don't sub negative or 0 funds.");
                 return;
             }
 
-            c.balance-=i;
+            c.balance -= i;
             await ReplyAsync(c.name + " has a new balance of " + c.balance);
             Character.update_character(c);
         }
 
         [Command("bal")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task balFundsAsync(SocketUser user) {
+        public async Task balFundsAsync(SocketUser user)
+        {
             var c = Character.get_character(user.Id);
-            if(c == null) {
+            if (c == null)
+            {
                 await ReplyAsync("This user doesn't have an account");
                 return;
             }
@@ -69,7 +120,8 @@ namespace trillbot.Commands
         }
 
         [Command("help")]
-        public async Task helpAsync() {
+        public async Task helpAsync()
+        {
             await Context.User.SendMessageAsync("Please check out this google document for my commands: <https://docs.google.com/document/d/1pWfIToswRCDVpqTK1Bj5Uv6s-n7zpOaqgZHQjW3SNzU/edit?usp=sharing>");
         }
 
