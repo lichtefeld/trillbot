a6a2cd48aeb03387c882c065e58795e971a10232
Jacob Lichtefeld <jalichtefeld@gmail.com>
2018-11-09 18:19:51 -0500

    Roulette Initial Commit

---
+++ Classes/cls_roulette.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using JsonFlatFileDataStore;
+using Newtonsoft.Json;
+using trillbot.Classes;
+
+namespace trillbot.Classes {
+
+    public class roulette {
+
+    }
+
+}
